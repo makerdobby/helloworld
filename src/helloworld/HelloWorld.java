41941322bfb5a52854383d97a34bea33ed408359
bit <bit@DESKTOP-487GS9K>
2020-05-08 20:43:07 +0900

    helloworld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2!"); 
+		
+		int number = 5+5-1;
+		
+		int number2 = ten();
+		
+		System.out.println(number2 + ":" + (number + 1));
+		
+	}
+	
+	public static int ten() {
+			return 10;
+	}
+
+}
